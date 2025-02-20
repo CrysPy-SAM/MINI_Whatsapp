4a09f2272387c9e630a5995af22a727e65acd878
Cryspysam <ssmishra9918@gmail.com>
2025-02-20 23:50:04 +0530

    Fixed missing chat.js module issue

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
 
-async function main(){
-    main().catch(err => console.log(err));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+  console.log("Connection successful");
 }
 
-} 
+main().catch((err) => console.log(err));
+
+let chat1 = new Chat({
+  from: "neha",
+  to: "priya",
+  message: "send me your exam sheets",
+  created_at: new Date(),
+});
+
+chat1.save().then((res) => {
+  console.log(res);
+});
 
 app.get("/", (req, res) => {
-    res.send("root is working");
-})
+  res.send("Root is working");
+});
 
-app.listen(8080, ()=>{
-    console.log("server is listening on port 8080");
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
